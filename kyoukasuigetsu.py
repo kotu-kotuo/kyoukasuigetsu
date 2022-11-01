e302297c145886d4ee828199e894b12af205006b
Nono <52486487+kotu-kotuo@users.noreply.github.com>
2022-11-01 11:28:37 +0900

    メッセージタスク追加

--- kyoukasuigetsu.py
+++ kyoukasuigetsu.py
@@ -43,8 +43,8 @@ homePage = os.environ["HOME_URL"]
 messagePage = os.environ["MESSAGE_URL"]
 
 
-# スケジュールタスク
-def task():
+# 提出タスク
+def submit():
   # ドライバーの起動
   driver = webdriver.Chrome(service=service, options=options)
   # 要素検索時のデフォルト待機時間(最大)
@@ -71,18 +71,18 @@ def task():
     sleep(3)
     if driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/a"):
       wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/a"))).click()
+      print("ボタンクリック")
     else:
       print("ボタンが押せませんでした")
       raise Exception
-    print("ボタンクリック")
     sleep(3)
     unixtime = str(int(time.time()))
     if driver.find_element(By.XPATH, "/html/body/div[14]/div[2]/form/div/p[2]/textarea"):
       driver.find_element(By.XPATH, "/html/body/div[14]/div[2]/form/div/p[2]/textarea").send_keys(message + unixtime)
+      print("入力完了")
     else:
       print("入力できませんでした")
       raise Exception
-    print("入力完了")
     sleep(3)
     if driver.find_elements(By.XPATH, "/html/body/div[14]/div[2]/form/div/p[5]/input[2]"):
       wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[14]/div[2]/form/div/p[5]/input[2]"))).click()
@@ -94,9 +94,53 @@ def task():
     print("例外処理" + traceback.format_exc())
   driver.quit()
 
+# メッセージタスク
+def sendMessage():
+  # ドライバーの起動
+  driver = webdriver.Chrome(service=service, options=options)
+  # 要素検索時のデフォルト待機時間(最大)
+  driver.implicitly_wait(1)
+  # 明示的な待機時間(最大)
+  wait = WebDriverWait(driver, 10)
+
+  print("スタート")
+  try:
+    driver.get(homePage)
+    sleep(3)
+    if driver.find_element(By.ID, "username"):
+      driver.find_element(By.ID, "username").send_keys(email)
+      print("ユーザーネーム入力")
+    if driver.find_element(By.ID, "password"):
+      driver.find_element(By.ID, "password").send_keys(password)
+      print("メールアドレス入力")
+    if driver.find_elements(By.CLASS_NAME, "button-login"):
+      wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "button-login"))).click()
+      print("ログイン完了")
+    sleep(3)
+    driver.get(messagePage)
+    print("ページ遷移")
+    sleep(3)
+    unixtime = str(int(time.time()))
+    if driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/div[5]/div/div/div[6]/div/form/textarea"):
+      driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/div[5]/div/div/div[6]/div/form/textarea").send_keys(message + unixtime)
+      print(message + unixtime)
+    else:
+      print("メッセージ入力できませんでした")
+      raise Exception
+    sleep(3)
+    if driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/div[5]/div/div/div[6]/div/form/div[3]/div[2]/div[1]/button"):
+      wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div/div[5]/div/div/div[6]/div/form/div[3]/div[2]/div[1]/button"))).click()
+      print("メッセージ送れました!!")
+    else:
+      print("メッセージ送れませんでした")
+      raise Exception
+  except Exception:
+    print("例外処理" + traceback.format_exc())
+  driver.quit()
 
 # スケジュール設定
-schedule.every(30).to(3000).seconds.do(task)
+schedule.every(30).to(3000).seconds.do(submit)
+schedule.every(10000).to(180000).seconds.do(sendMessage)
 
 #スケジュール実行
 while True:
